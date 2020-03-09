06e71e16e51d7a0efdeaa108569622422046f937
Katrusy86 <katrin.myronova@gmail.com>
2020-03-09 22:02:26 +0200

    Added a new work of js

---
+++ task1.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+console.log (user);
+
+const userKeys = Object.keys(user);
+
+for (const key of userKeys){
+    const value = user[key];
+    console.log(`${key}:${value}`);
+}
+
---
+++ task4.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const countTotalSalary = function(employees) {
+
+    let total = 0;
+
+    const salaryKeys = Object.keys(employees);
+
+    for (const key of salaryKeys) {
+        total+=employees[key];
+    }
+
+    return total
+};
+
+/*
+* Вызовы функции для проверки работоспособности твоей реализации.
+*/
+console.log(countTotalSalary({})); // 0
+
+console.log(
+countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+}),
+); // 330
+
+console.log(
+countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+}),
+); // 400
